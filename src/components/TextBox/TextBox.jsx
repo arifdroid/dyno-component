684ff4280556f56320ab21b6723b831e02de3dcf
arifdroid <arifdroiddeveloper@gmail.com>
2022-01-12 23:47:28 +0800

    renderprop pattern

--- src/components/TextBox/TextBox.jsx
+++ src/components/TextBox/TextBox.jsx
@@ -1,33 +1,62 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 
 //render prop, to enable dynamic control rendering
 
 class TextArea extends Component {
+
     state = { textValue: '' }
 
+    val = (e) => {
+        this.setState({
+            textValue: e.target.value
+        })
+    }
+
     render() {
-        return (
-            <TextField
-                id="standard-basic" variant="outlined" style={{ width: 200 }}
-            // value={textValue} 
-            />
-        )
+
+        return this.props.children({ textValueProp: this.val, inputValueProp: this.state.textValue })
     }
 }
 
+// const TextArea = (props) => {
+
+//     const [textValue, setTextValue] = useState('')
+
+//     return props.children({ textValueProp: setTextValue, inputValueProp: textValue })
+// }
+
+// const renderTextField = ({ textValue }) => {
+//     return (
+//         <TextField
+//             id="standard-basic" variant="outlined"
+//             style={{ width: 200 }}
+//             value={textValue}
+//         />
+//     )
+// }
 
 
-function TextBox() {
+
+function TextBox({ label }) {
+
     return (
-        <div>
-            <Typography>Account Number</Typography>
-            <TextField
-                id="standard-basic" variant="outlined" style={{ width: 200 }}
-            // value={textValue}
-            />
-        </div>
+
+        <TextArea >
+            {({ textValueProp, inputValueProp }) => {
+                
+                return (
+                    <>
+                        <TextField
+                            id="standard-basic" variant="outlined"
+                            style={{ width: 200 }}
+                            onChange={textValueProp}
+                        />
+                    </>
+                )
+            }}</TextArea>
+
 
     )
 }
